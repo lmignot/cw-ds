6f8f24a7664bf6b2d62598e9b7a94a0705b91d4f
Laurent Mignot <laurent@mignot.eu>
2015-12-07 21:39:30 +0000

    initial class file for FunctionalArrayList

---
+++ FunctionalArrayList.java
@@ -0,0 +1,16 @@
+
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+    /**
+     * @see FunctionalList#head()
+     */
+    public ReturnObject head () {
+
+    }
+
+    /**
+     * @see FunctionalList#rest()
+     */
+    public FunctionalList rest () {
+
+    }
+}
