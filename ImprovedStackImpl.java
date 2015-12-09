91b3c6ba42850daf7e1375ab92ed2d4e5e54be3c
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2015-12-09 22:35:44 +0000

    initial commit of ImprovedStackImpl

---
+++ ImprovedStackImpl.java
@@ -0,0 +1,23 @@
+/**
+ * Implementation of an ImprovedStack
+ *
+ * @author Laurent Mignot
+ */
+public class ImprovedStackImpl implements ImprovedStack {
+
+    public ImprovedStackImpl () {
+        
+    }
+
+    /**
+     * @see ImprovedStack#reverse()
+     */
+    public ImprovedStack reverse() {
+        return new ImprovedStackImpl();
+    };
+
+    /**
+     * @see ImprovedStack#remove()
+     */
+    public void remove(Object object);
+}
